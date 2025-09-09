f36f5fea991d6b7f4e4f7c37bc797baac283d018
muriloact <muriloact@hotmail.com>
2025-09-08 23:32:34 -0400

    Test de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./assets/pages/home";
-import Details from "./assets/pages/Details";
-import NotFound from "./assets/pages/NotFound";
-import Navbar from "./assets/components/Navbar";
+import Home from "./assets/pages/Home.jsx";
+import Details from "./assets/pages/Details.jsx";
+import NotFound from "./assets/pages/NotFound.jsx";
+import Navbar from "./assets/components/Navbar.jsx";
 export default function App() {
 return (
 <Router>
@@ -17,19 +17,3 @@ return (
 </Router>
 );
 }
-
-
-
-// import './App.css'
-// import Home from './assets/pages/home'
-
-// function App() {
-
-//   return (
-//     <>
-//       <Home></Home>
-//     </>
-//   )
-// }
-
-// export default App
